d6beee624cad339f0b59e026e912947dca8c98cd
kyle-bdvl <kyle.boudville@gmail.com>
2024-10-08 18:49:21 +0800

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,22 @@
+//all global  variables
+
+//this is for displaying all the buttons and functions onto the screen 
+
+function add (num1 , num2){
+  return num1 +num2 ;
+}
+
+function subtract (num1, num2){
+  return num1 -num2;
+}
+
+function multiply (num1, num2){
+  return num1*num2;
+}
+
+function divide (num1, num2){
+  if (num2==0)
+    throw new Error ('Error:division by 0 is not allowed');
+  return num1/ num2;
+}
+//to make the buttons appear on screen 
