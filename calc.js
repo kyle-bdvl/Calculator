07ad9bf86207393e32fad48aeb0818dd6993bbf8
Kyle <kyle.boudville@gmail.com>
2024-10-09 13:35:32 +0800

    the functions are working but the clear function isn't working currently

--- calc.js
+++ calc.js
@@ -1,26 +1,69 @@
 //all global  variables
+let firstNumber;
+let secondNumber;
+let step =0;
+let operation;
+let result=0;
+
+//the array for the calculations 
+let numArray =[];
+let secondNumArray =[];
+//display 
+let display= document.getElementById('display');
 
 //this is for displaying all the buttons and functions onto the screen 
 
-function add (num1 , num2){
-  return num1 +num2 ;
+function getNumber(num){
+  if(step===0 || step===1){
+    numArray.push(num);
+    step=1;
+    firstNumber= Number(numArray.join('')); //merge into one string, then into number
+    display.value= firstNumber;
+  }
+  else if (step ===2){
+    secondNumArray.push(num);
+    secondNumber = Number(secondNumArray.join(''));
+    display.value=secondNumber;
+  }
+  
 }
 
-function subtract (num1, num2){
-  return num1 -num2;
+function getOperator(op){
+  step =2 
+  operation =op;
+}
+//to clear all 
+function clear(){ 
+  firstNumber = 0;
+  secondNumber =0;
+  step =0;
+  operation = 0;
+  result=0;
+  numArray=[];
+  secondNumArray=[];
+  console.log('display');
 }
 
-function multiply (num1, num2){
-  return num1*num2;
+let calculatorEquals = ()=>{
+  if (operation ==='+'){
+    result = firstNumber + secondNumber;
+    display.value = result;
+  }
+
+  else if (operation==='-'){
+    result = firstNumber - secondNumber;
+    display.value = result;
+  }
+  else if (operation ==='*'){
+    result = firstNumber * secondNumber;
+    display.value = result;
+  }
+  else {
+    if (secondNumber === 0){
+      throw new Error('cannot divide by zero ');
+    }
+    result = firstNumber / secondNumber;
+    display.value = result;
+  }
 }
 
-function divide (num1, num2){
-  if (num2==0)
-    throw new Error ('Error:division by 0 is not allowed');
-  return num1/ num2;
-}
-
-const answer = document.getElementById("answer");
-const display = document.getElementById("question");
-//some code to commit to git hub
-//to make the buttons appear on screen 
